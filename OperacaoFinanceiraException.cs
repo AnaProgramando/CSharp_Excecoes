62c2cf4c3f5395e9519bc3f7ee0ad8fd21fd3506
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2023-04-08 19:31:20 -0300

    Create OperacaoFinanceiraException.cs

---
+++ OperacaoFinanceiraException.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Excecoes
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException()
+        {
+
+        }
+
+        public OperacaoFinanceiraException(string mensagem)
+            : base(mensagem)
+        {
+
+        }
+
+        public OperacaoFinanceiraException(string mensagem, Exception excecaoInterna)
+            : base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
